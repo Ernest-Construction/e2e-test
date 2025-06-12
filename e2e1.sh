68f86c781393d6ee96f7f7e40560a21cb84cbf16
Azeez Olaniran <azeezolaniran2016@gmail.com>
2025-06-12 05:32:00 +0100

    test-4

---
+++ e2e1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "$(basename "$0")"
+echo "Running e2e1.sh script"
+sleep 120
+exit 0
---
+++ e2e2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "$(basename "$0")"
+echo "Running e2e2.sh script"
+sleep 120
+exit 0
